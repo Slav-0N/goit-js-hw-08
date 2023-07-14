ffb11443fb4d4ccc0acc0be2298976e5177940ff
Slav-0N <SOVILGO@gmail.com>
2023-07-14 19:07:52 +0300

    убрал точки списка

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,14 @@
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css
+
 
 console.log(galleryItems);
 
@@ -37,4 +38,7 @@ galleryElem.insertAdjacentHTML("afterbegin", template);
 //   captionDelay: 250,
 // });
 
-
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
