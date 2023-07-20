c6c1b026efd59f6bea41d2f45f8cca3e321f4de2
Slav-0N <SOVILGO@gmail.com>
2023-07-20 13:09:35 +0300

    виправив трет

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,70 +1,110 @@
 
 import throttle from "lodash.throttle";
-const ref = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input'),
-  message: document.querySelector('textarea'),
-};
+const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const formObjEl = {};
-console.log(formObjEl);
+let formObjEl = {};
 
 
-enteredText();
 
-ref.message.addEventListener('input', throttle(showTextareaInput, 500));
+form.addEventListener('input', throttle(showTextareaInput, 500));
 
 function showTextareaInput(event) {
-  // const funcMessage = event.target.value;
-  // console.log(funcMessage);
-  // console.log(event.target.name);
-  formObjEl[event.target.name] = event.target.value;
-  // console.log(formObjEl[event.target.name] = event.target.value);
-  // console.log(formObjEl);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObjEl));
-  // localStorage.setItem(STORAGE_KEY, funcMessage);
-  // console.log(formObjEl);
 
+  if (event.target.name === 'message') {
+    formObjEl[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formObjEl));
+  } else if(event.target.name === 'email') {     
+  formObjEl[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObjEl));
+  };
 };
 
-ref.email.addEventListener('input', throttle(showEmail,500));
 
-function showEmail(event) { 
-  // const email = event.currentTarget.value;
-  formObjEl[event.target.name] = event.target.value;
-  // console.log(formObjEl[event.target.name] = event.target.value);
-  // console.log(formObjEl);
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObjEl));
-  // console.log(email);
-
-};
-
-ref.form.addEventListener('submit', submitClick);
-
+form.addEventListener('submit', submitClick);
 
 function submitClick(event) {
   event.preventDefault();
+  
+  const { elements: { email, message } } = event.currentTarget;
+  
+  if (email.value === '' || message.value === '') {
+    alert('Всі поля повинні бути заповнені');
+  } else {
+    console.log(formObjEl);
 
-  const finalObj = localStorage.getItem(STORAGE_KEY);
-  // console.log(finalObj);
-  console.log(JSON.parse(finalObj));
-  // console.log('Форма отправлена, поля очищены');
-  event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+    formObjEl = {};
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  };
+
 };
 
 
-function enteredText() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
 
-  if (savedMessage) {
-    // console.log(savedMessage);
-    const recoveredFormObjEl = JSON.parse(savedMessage);
-    // console.log(recoveredFormObjEl);
-    ref.message.value = recoveredFormObjEl.message;
-    ref.email.value = recoveredFormObjEl.email;
-  }
-};
+
+
+
+
+
+
+
+
+// ==========================================================================
+// enteredText();
+
+// ref.message.addEventListener('input', throttle(showTextareaInput, 500));
+
+// function showTextareaInput(event) {
+//   // const funcMessage = event.target.value;
+//   // console.log(funcMessage);
+//   // console.log(event.target.name);
+//   formObjEl[event.target.name] = event.target.value;
+//   // console.log(formObjEl[event.target.name] = event.target.value);
+//   // console.log(formObjEl);
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formObjEl));
+//   // localStorage.setItem(STORAGE_KEY, funcMessage);
+//   // console.log(formObjEl);
+
+// };
+
+// ref.email.addEventListener('input', throttle(showEmail,500));
+
+// function showEmail(event) { 
+//   // const email = event.currentTarget.value;
+//   formObjEl[event.target.name] = event.target.value;
+//   // console.log(formObjEl[event.target.name] = event.target.value);
+//   // console.log(formObjEl);
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formObjEl));
+//   // console.log(email);
+
+// };
+
+// ref.form.addEventListener('submit', submitClick);
+
+
+// function submitClick(event) {
+//   event.preventDefault();
+
+//   const finalObj = localStorage.getItem(STORAGE_KEY);
+//   // console.log(finalObj);
+//   console.log(JSON.parse(finalObj));
+//   // console.log('Форма отправлена, поля очищены');
+//   event.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// };
+
+
+// function enteredText() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//   if (savedMessage) {
+//     // console.log(savedMessage);
+//     const recoveredFormObjEl = JSON.parse(savedMessage);
+//     // console.log(recoveredFormObjEl);
+//     ref.message.value = recoveredFormObjEl.message;
+//     ref.email.value = recoveredFormObjEl.email;
+//   }
+// };
 
 
 
